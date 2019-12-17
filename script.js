c19faf808620474c492bd98f827098f31596abc8
Phillip Jones <ptj92e@gmail.com>
2019-12-16 19:09:35 -0600

    Adjusted styling

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 $(document).ready(function () {
-   let welcomeDiv = $("#welcome");
-   let bioDiv = $(".bioDiv");
-   let portfolioDiv = $(".portfolioDiv");
-   let contactDiv = $(".contactDiv");
-   
    let controller = new ScrollMagic.Controller();
+
+   let welcomeFade = ScrollMagic.Scene({
+      triggerElement: ".welcomeDiv"
+   })
+      .setClassToggle(".welcomeDiv", "fade-out")
+      .addTo(controller);
 
    let bioFade = new ScrollMagic.Scene({
       triggerElement: ".bioDiv"
    })
-   .setClassToggle(".bioDiv", "fade-in")
-   .addTo(controller);
+      .setClassToggle(".bioDiv", "fade-in")
+      .addTo(controller);
 
    let portfolioFade = new ScrollMagic.Scene({
       triggerElement: ".portfolioDiv"
    })
-   .setClassToggle(".portfolioDiv", "fade-in")
-   .addTo(controller);
+      .setClassToggle(".portfolioDiv", "fade-in")
+      .addTo(controller);
 
    let contactFade = new ScrollMagic.Scene({
       triggerElement: ".contactDiv"
    })
-   .setClassToggle(".contactDiv", "fade-in")
-   .addTo(controller);
+      .setClassToggle(".contactDiv", "fade-in")
+      .addTo(controller);
+
+   function offsetAnchor() {
+      if (location.hash.length !== 0) {
+         window.scrollTo(window.scrollX, window.scrollY - 1000);
+      };
+   };
+
+   $("#bioLink").click(offsetAnchor());
+
 });
